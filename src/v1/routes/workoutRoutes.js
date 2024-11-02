0c9ff50dd07922fa03c5ff72ed7b923efe0cbde8
aks151 <kumar.ayush151@gmail.com>
2024-11-02 12:14:28 +0530

    creating the first service(workout)

---
+++ src/v1/routes/workoutRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const router = express.Router();
+
+const workoutController = require("../../controllers/workoutControllers")
+
+router.get("/", workoutController.getAllWorkouts)
+
+router.get("/:workoutId", workoutController.getOneWorkout)
+
+router.post("/", workoutController.createNewWorkout)
+
+router.patch("/:workoutId", workoutController.updateOneWorkout)
+
+router.delete("/:workoutId", workoutController.deleteOneWorkout)
+
+module.exports = router;
